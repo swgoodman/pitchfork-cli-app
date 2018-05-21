8c11cb93c4693eb54ed3516e26a61dc45ba7791c
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-21 08:35:08 -0700

    Hardcoding CLI.

--- lib/pitchforkCLI/cli.rb
+++ lib/pitchforkCLI/cli.rb
@@ -11,24 +11,47 @@ class PitchforkCLI::CLI
   end
 
   def greeting
+    puts "                       "
     puts "-----------------------"
     puts "Pitchfork | Music Lists"
     puts "'The most trusted voice in music.'"
     puts "-----------------------"
     puts "                       "
-    puts "Select a year below to see its top five albums according to Pitchfork.com."
   end
 
   def list_lists
     # pulls in info from scraper, instatiates new lists, and prints list of lists
+    puts "Select a year below to see its top five albums according to Pitchfork.com."
+    # @lists = PitchforkCLI::Lists.all
+    puts "-----------------------"
+    puts "1. 2016"
+    puts "2. 2015"
+    puts "3. 2014"
+    puts "4. 2013"
   end
 
   def menu
     # gets input from user (chosen list, exit)
+    puts "Enter the corresponding number to see the lists' top five albums:"
+    input = nil
+      while input != "exit"
+        input = gets.strip
+        case input
+        when "1"
+          puts "2016 top 5"
+        when "2"
+          puts "2015 top 5"
+        when "3"
+          puts "2014 top 5"
+        when "4"
+          puts "2013 top 5"
+        end
+      end
   end
 
   def goodbye
-    # prints goodbye message
+    puts "Thanks for stopping by."
+    puts "-----------------------"
   end
 
 end
