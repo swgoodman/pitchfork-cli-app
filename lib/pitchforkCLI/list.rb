de0ff8b7eb9387c977876156d26f5ad470bbf74e
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-21 12:26:52 -0700

    Scraper partially working.

--- lib/pitchforkCLI/list.rb
+++ lib/pitchforkCLI/list.rb
@@ -1,43 +1,37 @@
 
 class PitchforkCLI::List
 
-  attr_accessor :year, :top_five, :URL
+  attr_accessor :year, :top_five, :url
 
   @@all = []
 
-  def initialize(year)
-    @year = year
-    @top_five = "THIS IS THE TOP 5"
-    # @@all << self
+  def initialize
+    @year
+    @top_five
+    @url
   end
 
   def self.all
     # returns all instances of lists
     @@all
+    puts "this is working"
   end
 
   def self.scrape_list
     #scrape's list title from site
     doc = Nokogiri::HTML(open("https://pitchfork.com"))
+
+    list = doc.search("span.year")[1..11].text.scan(/..../)
+
+    list.each.with_index do |list, i|
+      list = self.new
+      list.year = doc.search("span.year")[i].text
+      @@all << list
+    end
     binding.pry
-
-    lists = doc.search("div.lists").text
-
-    year = doc.search("span.year")[1].text
-
-    list.each do |list|
-      list = self.new()
-
-      
-
-
-
   end
 
   def self.scrape_top_five
     #scrape's top five from each list
-
   end
-
-
 end
