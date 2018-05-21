854308c501a000b940326e7a9cf6a00d5fc1a74e
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-21 08:59:46 -0700

    Hardcoding CLI interface.

--- lib/pitchforkCLI/cli.rb
+++ lib/pitchforkCLI/cli.rb
@@ -12,10 +12,10 @@ class PitchforkCLI::CLI
 
   def greeting
     puts "                       "
-    puts "-----------------------"
+    puts "----------------------------------"
     puts "Pitchfork | Music Lists"
     puts "'The most trusted voice in music.'"
-    puts "-----------------------"
+    puts "----------------------------------"
     puts "                       "
   end
 
@@ -23,7 +23,6 @@ class PitchforkCLI::CLI
     # pulls in info from scraper, instatiates new lists, and prints list of lists
     puts "Select a year below to see its top five albums according to Pitchfork.com."
     # @lists = PitchforkCLI::Lists.all
-    puts "-----------------------"
     puts "1. 2016"
     puts "2. 2015"
     puts "3. 2014"
@@ -32,24 +31,45 @@ class PitchforkCLI::CLI
 
   def menu
     # gets input from user (chosen list, exit)
-    puts "Enter the corresponding number to see the lists' top five albums:"
+    puts "                       "
+    puts "Enter the corresponding number to see the lists' top five albums."
+    puts "(Enter 'exit' to leave.)"
     input = nil
-      while input != "exit"
+      until input == "exit"
         input = gets.strip
-        case input
-        when "1"
-          puts "2016 top 5"
-        when "2"
-          puts "2015 top 5"
-        when "3"
-          puts "2014 top 5"
-        when "4"
-          puts "2013 top 5"
-        end
+          case input
+          when "1"
+            puts "2016 top 5"
+            puts "                       "
+            puts "Type 'list' to choose another list or type 'exit' to leave."
+          when "2"
+            puts "2015 top 5"
+            puts "                       "
+            puts "Type 'list' to choose another list or type 'exit' to leave."
+          when "3"
+            puts "2014 top 5"
+            puts "                       "
+            puts "Type 'list' to choose another list or type 'exit' to leave."
+          when "4"
+            puts "2013 top 5"
+            puts "                       "
+            puts "Type 'list' to choose another list or type 'exit' to leave."
+          when "list"
+            puts "                       "
+            puts "Enter the corresponding number to see the lists' top five albums."
+            puts "(Enter 'exit' to leave.)"
+            puts "1. 2016"
+            puts "2. 2015"
+            puts "3. 2014"
+            puts "4. 2013"
+          else
+            puts "Invalid entry."
+          end
       end
   end
 
   def goodbye
+    puts "                       "
     puts "Thanks for stopping by."
     puts "-----------------------"
   end
