96852206eb999d7c8cbcb2908d0f491a013c3348
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-20 22:53:22 -0700

    Added CLI greeting.

--- lib/pitchforkCLI.rb
+++ lib/pitchforkCLI.rb
@@ -1,6 +1,8 @@
 require_relative "pitchforkCLI/version"
 require_relative "./pitchforkCLI/cli"
 
+require 'pry'
+
 module PitchforkCLI
   # Your code goes here...
 end
--- lib/pitchforkCLI/cli.rb
+++ lib/pitchforkCLI/cli.rb
@@ -1,8 +1,24 @@
 
+require 'pry'
+
 class PitchforkCLI::CLI
 
   def call
-    
+    greeting
+    list_lists
+  end
+
+  def greeting
+    puts "-----------------------"
+    puts "Pitchfork | Music Lists"
+    puts "'The most trusted voice in music.'"
+    puts "-----------------------"
+    puts "                       "
+    puts "Select a year below to see its top five albums according to Pitchfork.com."
+  end
+
+  def list_lists
+
   end
 
 end
