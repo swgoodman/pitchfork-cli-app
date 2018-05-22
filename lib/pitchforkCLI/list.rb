4f9039a0bdba75f60092c8faefa9fbdbc1c184b0
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-21 17:16:46 -0700

    Working on scraper.

--- lib/pitchforkCLI/list.rb
+++ lib/pitchforkCLI/list.rb
@@ -14,21 +14,26 @@ class PitchforkCLI::List
   def self.all
     # returns all instances of lists
     @@all
-    puts "this is working"
   end
 
   def self.scrape_list
     #scrape's list title from site
     doc = Nokogiri::HTML(open("https://pitchfork.com"))
 
-    list = doc.search("span.year")[1..11].text.scan(/..../)
+        binding.pry
+
+    list = doc.search("span.year")[1..10].text.scan(/..../)
+    urls = doc.at_css('div.lists a:contains("Albums")')['href']
+    url_gang = urls.tap {|link| link["href"]}
+
 
     list.each.with_index do |list, i|
       list = self.new
-      list.year = doc.search("span.year")[i].text
+      list.year = doc.search("span.year")[i+1].text
+      list.url = doc.at_xpath('//a[text()="Albums"]')
       @@all << list
     end
-    binding.pry
+
   end
 
   def self.scrape_top_five
